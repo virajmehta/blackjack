78370bf24888a437f9de8a3e53271dc008ced15c
Viraj Mehta <thebigmehtaphor@gmail.com>
2015-07-20 22:06:25 -0700

    great success

---
+++ Blackjack.py
@@ -0,0 +1,24 @@
+
+numDecks = 1
+numGames = 1
+deck
+
+
+def readOptions():
+
+
+def setUpState():
+
+
+def main():
+	readOptions()
+	setUpState()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
