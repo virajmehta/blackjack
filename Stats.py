c1bcecd1a16cb176344f3d4958a1b326d3ccac53
Viraj Mehta <thebigmehtaphor@gmail.com>
2015-07-30 00:17:55 -0700

    decisions can be made

--- Stats.py
+++ Stats.py
@@ -4,26 +4,61 @@ import random
 class Entry:
 	'''Contains a single entry in the matrix of games played. Starts with 10 to dampen the initial effect of games'''
 	def __init__(self, isSplit):
-		self.numPlayedHit = 10
-		self.earningsHit = 0
-		self.numPlayedStand = 10
-		self.earningsStand = 0
-		self.numPlayedDouble = 10
-		self.earningsDouble = 0
-		self.numPlayedSplit = 10 if isSplit else 0
-		self.earningsSplit = 0
+		self.numPlayed = [10 for x in range(0, 4)]
+		self.earnings = [0 for x in range(0, 4)]
+		
 
-	def addPlay(self, earnings):
-		self.numPlayed += 1
-		self.earnings += earnings
+	def addPlay(self, earnings, play):
+		self.numPlayed[play] += 1
+		self.earnings[play] += earnings
 
-	def getExpectedValue(self):
-		return self.earnings / self.numPlayed
+	def getExpectedValue(self, play):
+		return self.earnings[play] / self.numPlayed[play]
+
+	def randomize(self, probs):
+		sumProb = 0
+		for play in range(0, 4):
+			sumProb += probs[play]
+			probs[play] = sumProb
+		myAss = random.random()
+
+		for play in range(0, 4):
+			if myAss < probs[play]
+				return play
+
+		return -1
 
 	def getNextPlay(self):
 		
+		expectedValue = []
+		for play in range(0, 4):	#update EV
+			expectedValue.append(self.getExpectedValue(play))
+
+		probs = [0 for x in range(0, 4)]
+		remainingProb = 1.0
+
+
+		for rank in range(4, 1, -1):
+			minVal = 3 	#arbitrary big value
+			minPlay = 4	#not a value
+			for play in range(0, 4):
+				if expectedValue[play] < minVal:
+					minVal = expectedValue[play]
+					minPlay = play
+			expectedValue[minPlay] = 4 	#arbitrary bigger value
+			probs[minPlay] = remainingProb / (rank * ((1 + minVal) ** 2))
+
+		for play in range(0, 4):
+			if expectedValue[play] != 4:
+				probs[play] = remainingProb
+				break
+
+		return self.randomize(probs)
+		
+
 
 class Stats:
 	
 	def __init__(self):
+		#TODO: initialize stats model and handke interface
 		
